e946d6fbeee54d9c936ef20f445cfc027bf8024a
Eugen Pavlov <Eugen.Pavlov@gmail.com>
2017-01-16 21:09:23 +0200

    added hw2

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+var arr = [];
+
+while (true) {
+
+  var nameList = prompt("Enter name", 'name');
+
+  if (nameList === "" || nameList === null || isNaN(nameList)) break;
+
+  arr.push(+value);
+  console.log(arr);
+}
