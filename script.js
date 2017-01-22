e4c2ec04fa046949da8169de545f4c4b1c6604c3
Eugen Pavlov <Eugen.Pavlov@gmail.com>
2017-01-22 20:33:26 +0200

    added hw2

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 
-
+var namesCount = 5;
 var arr = [];
+arr.length = namesCount;
 
-while (true) {
-
-  var nameList = prompt("Enter name", 'name');
-
-  if (nameList === "" || nameList === null || isNaN(nameList)) break;
-
-  arr.push(+value);
-  console.log(arr);
+for (var i = 0; i < namesCount; i++) {
+	arr[i] = prompt("Enter your name:", '');
+	if (arr[i] == null) break;
 }
+
+var msg = 'Access Denided';
+var findName = prompt('Enter login');
+
+if (findName !== null) {
+	for (var i = 0; i < namesCount; i++) {
+		if (arr[i] == findName) {
+		msg = 'Welcome ' + findName + '!';
+		}
+	}
+}
+alert(msg);
